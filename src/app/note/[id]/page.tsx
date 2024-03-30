6f1b3d0a5efc25a41f6bdd025aa6531e5b70daa7
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-30 20:34:03 +0530

    feat: cloud sync notes

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,25 +1,38 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Editor } from "novel";
 import { getNoteById } from "@/app/actions";
 
-export default function page() {
-  useEffect(() => {
+type Note = {
+  content: string;
+  id: number;
+  title: string;
+  userId: number;
+};
+
+export default function Page() {
+const [note, setNote] = useState<Note | null>(null);
+
+useEffect(() => {
     const fetchData = async () => {
-      try {
-        const notesData: any = await getNoteById(1);
-        console.log(notesData);
-      } catch (error) {
-        console.error(error);
-      }
+        try {
+            const notesData: Note[] = await getNoteById(1);
+            console.log(notesData[0].content);
+            setNote(notesData[0]); 
+            
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     fetchData();
-  }, []);
+}, []);
 
   return (
     <>
       <Editor
+        defaultValue={note ? note.content : 'No note found'} 
+        key={note?.content}
         completionApi="/api/generate"
         className="text-gray-100 bg-black h-[100vh]"
       />
