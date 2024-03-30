8a430b57b12545b2042f5a8c56ee5653d81c94a7
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-30 16:50:08 +0530

    feat: user crud

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,18 @@
 'use server'
 
-async function createUser(){
-    
+import { db } from "@/utils/db/client"
+import { users } from "@/utils/db/schema"
+import { UserType } from "@/utils/types"
+import { eq } from 'drizzle-orm';
+
+export async function createUser(user: UserType){
+    return db.insert(users).values(user).returning();
 }
+
+export async function updateUser(user: UserType) {
+    return db.update(users).set(user).where(eq(users.id, user.id)).returning();
+  }
+  
+  export async function deleteUser(id: number) {
+    return db.delete(users).where(eq(users.id, id)).returning();
+  }
---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export type UserType = {
+    id: number,
+    email: string,
+    username: string
+}
+
+export type NoteType = {
+    title: string,
+    content: string,
+    userId: number
+}
