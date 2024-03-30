ca575a75fe923c59d57344d40b0b4ecf03e25691
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-30 15:23:04 +0530

    fix: /route

---
+++ src/app/api/generate/route.ts
@@ -0,0 +1,42 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { env } from "process";
+
+export const runtime = "edge";
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request): Promise<Response> {
+  const { prompt } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that continues existing text based on context from prior text. " +
+          "Give more weight/priority to the later characters than the beginning ones. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences. Just output in text format.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    n: 1,
+  });
+
+
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+}
