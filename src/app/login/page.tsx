faa10d51e0aebcbbf2c3835e120d3f7d79a01997
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-31 18:13:21 +0530

    fix: ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,39 @@
 import { login, signup } from "./actions";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      {/* add zod so min 6 characters is required */}
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex justify-center min-w-80">
+      <form>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          className="my-4"
+        />
+
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          className="my-4"
+        />
+        {/* add zod so min 6 characters is required */}
+        <div className="">
+          <Button formAction={login} className="my-4">
+            Log in
+          </Button>
+          <div>OR</div>
+          <Button formAction={signup} className="my-4">
+            Sign up
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,43 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="bg-slate-50 min-h-screen px-12">
+    <main className="bg-background min-h-screen px-12">
       <div className="w-full">
         <header className="flex justify-between items-center py-8 px-6">
-          <h1 className="text-3xl font-bold text-gray-800">
-            <span className="text-red-500">PIX</span>EL
+          <h1 className="text-3xl font-bold text-foreground">
+            <span className="text-primary">PIX</span>EL
           </h1>
           <nav>
             <ul className="flex space-x-4">
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-800">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/theanuragdev" className="text-gray-600 hover:text-gray-800">
-                  Team
-                </a>
-              </li>
-              <li>
-                <Link href="https://github.com/anuragts" className="text-gray-600 hover:text-gray-800">
-                  Opensource
+                <Link href="/notes" className="text-foreground hover:text-primary-foreground">
+                  My Notes
                 </Link>
               </li>
+              <li>
+                <Link href="/login" className="text-foreground hover:text-primary-foreground">
+                  Login
+                </Link>
+              </li>
+              {/* <li>
+                <Link href="" className="text-foreground hover:text-primary-foreground">
+                  github
+                </Link>
+              </li> */}
             </ul>
           </nav>
         </header>
         <div className="flex flex-col md:flex-row items-center md:justify-between py-16">
           <section className="text-center md:text-left p-8 md:w-1/2">
-            <h2 className="text-5xl font-bold text-gray-800 mb-4">
+            <h2 className="text-5xl font-bold text-secondary-foreground mb-4">
               Forget about your messy Notes.
             </h2>
-            <p className="text-gray-600 mb-8">
+            <p className="text-primary mb-8">
               AI-powered note-taking app designed to supercharge <br />
               your note-taking process.
             </p>
             <Link href={'/notes'}>
-            <button className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600">
+            <button className="bg-primary text-white font-bold py-2 px-4 rounded hover:bg-primary-foreground hover:text-primary">
               Try now
             </button>
             </Link>
