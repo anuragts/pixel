2147d1339208bb9c07493513667281c07ecfbd52
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-31 10:28:02 +0530

    fix: comment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ export default function LoginPage() {
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
+      {/* add zod so min 6 characters is required */}
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
